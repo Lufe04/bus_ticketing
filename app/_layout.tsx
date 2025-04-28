1b0ed88662392ef4c0384f91473be3587ae2397f
MarianaValle22 <marianavamo@unisabana.edu.co>
2025-04-27 20:31:08 -0500

    First commit for the driver-flow

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -9,6 +9,7 @@ export default function RootLayout() {
       >
         <Stack.Screen name="auth"/>
         <Stack.Screen name="(app)"/>
+        <Stack.Screen name="index"/>
       </Stack>
     </AuthProvider>
   )
